add1a2115d82ccc8c359cf03f74c57d283cb5fac
Dave Carlile <dave.carlile@sublimemedia.com>
2021-04-06 17:24:31 -0700

    exit with error

--- postinstall.js
+++ postinstall.js
@@ -1,4 +1,6 @@
-console.error(process.env.INIT_CWD)
-console.error(__dirname)
-console.error(process.cwd())
-console.error(process.env.PWD)
+console.log(process.env.INIT_CWD)
+console.log(__dirname)
+console.log(process.cwd())
+console.log(process.env.PWD)
+// get it to print to the console:
+process.exit(1)
