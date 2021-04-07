38f5ddf5070ae070727bd5feab890849ee79d089
Dave Carlile <dave.carlile@sublimemedia.com>
2021-04-06 18:08:36 -0700

    ZXC

--- postinstall.js
+++ postinstall.js
@@ -1,7 +1,8 @@
 console.log('starting postingstall.js')
 console.log('__dirname)', __dirname)
 console.log('process.cwd()', process.cwd())
-
+pak = require(process.env.npm_package_json)
+console.log(JSON.stringify(pak))
 var env = process.env;
 
 Object.keys(env).forEach(function(key) {
