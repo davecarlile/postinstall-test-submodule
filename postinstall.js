9a40497abe1f41a1f25e713d61122262363a99d4
Dave Carlile <dave.carlile@sublimemedia.com>
2021-04-06 17:50:14 -0700

    sadf

--- postinstall.js
+++ postinstall.js
@@ -1,8 +1,12 @@
 console.log('starting postingstall.js')
-console.log('process.env.INIT_CWD', process.env.INIT_CWD)
-console.log('process.env.npm_package_json', process.env.npm_package_json)
 console.log('__dirname)', __dirname)
 console.log('process.cwd()', process.cwd())
+
+var env = process.env;
+
+Object.keys(env).forEach(function(key) {
+  console.log('export ' + key + '="' + env[key] +'"');
+});
 
 // get it to print to the console: 
 process.exit(1)
