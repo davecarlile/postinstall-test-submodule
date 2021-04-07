ef89f7f1370337afa76d3044a1858a24540ad3a8
Dave Carlile <dave.carlile@sublimemedia.com>
2021-04-06 17:48:07 -0700

    asd

--- postinstall.js
+++ postinstall.js
@@ -1,7 +1,8 @@
 console.log('starting postingstall.js')
-console.log(process.env.INIT_CWD)
-console.log(__dirname)
-console.log(process.cwd())
+console.log('process.env.INIT_CWD', process.env.INIT_CWD)
+console.log('process.env.npm_package_json', process.env.npm_package_json)
+console.log('__dirname)', __dirname)
+console.log('process.cwd()', process.cwd())
 
-// get it to print to the console:
+// get it to print to the console: 
 process.exit(1)
