1dc1189b0b5683dc6621ae94a2d75f2ee34ee9fb
Dave Carlile <dave.carlile@sublimemedia.com>
2021-04-06 17:31:21 -0700

    added message to postinstall

--- postinstall.js
+++ postinstall.js
@@ -1,6 +1,7 @@
+console.log('starting postingstall.js')
 console.log(process.env.INIT_CWD)
 console.log(__dirname)
 console.log(process.cwd())
-console.log(process.env.PWD)
+
 // get it to print to the console:
 process.exit(1)
